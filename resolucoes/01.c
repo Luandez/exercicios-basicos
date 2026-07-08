62aa871d9bd1ad1c94fb4a8c45c03cac11cd7f27
Luan <ls2386006@gmail.com>
2026-07-08 16:50:03 -0400

    exercicio converte temperatura

--- resolucoes/01.c
+++ resolucoes/01.c
@@ -1,11 +1,16 @@
+//Escreva um algoritmo para ler uma temperatura em graus fahrenheit, calcular e escrever o valor correspondente em graus celcius:
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-    for (int i = 0; i < 10; i++)
-    {
-        printf("Hello world!\n");
-    }
+    float c, f;
+    printf("Entre com a temperatura em F: ");
+    fflush(stdout);
+    scanf("%f", &f);
+
+    c = (f - 32) * 5/9;
+    printf("A temperatura de %.2fF convertida para celsius é %.2fC.\n", f, c);
     return 0;
 }
